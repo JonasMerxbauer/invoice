623451ef99f019905b176e1c17101a3c4f1fea5b
Jonáš Merxbauer <jonasmerxbauer@outlook.com>
2026-01-10 22:57:42 +0100

    basic evolu setup

--- src/evolu/index.ts
+++ src/evolu/index.ts
@@ -5,7 +5,6 @@ import { createUseEvolu } from "@evolu/react";
 
 export const evolu = createEvolu(evoluReactWebDeps)(Schema, {
   name: SimpleName.orThrow("invoice"),
-  transports: [{ type: "WebSocket", url: "wss://your-sync-url" }], // optional, defaults to free.evoluhq.com
 });
 
 export const useEvolu = createUseEvolu(evolu);
